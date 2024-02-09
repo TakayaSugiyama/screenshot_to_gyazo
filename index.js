38014fd13f4bf3e4d1d8e4af4da5c688665ec5d4
takayasugiyama <s.takaya1027@gmail.com>
2024-02-10 04:03:44 +0900

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+import chokidar from "chokidar";
+import { readFileSync } from "fs";
+import clipboard from "clipboardy";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
+
+const watcher = chokidar.watch(process.env.WATCH_DIRECTORY, {
+  ignoreInitial: true,
+  awaitWriteFinish: true,
+});
+
+const buildFormData = (path) => {
+  const formData = new FormData();
+  formData.append("access_token", `${process.env.TOKEN}`);
+  const file = readFileSync(path);
+  formData.append("imagedata", new Blob([file], { type: "image/png" }), {
+    filename: path,
+  });
+  return formData;
+};
+
+const uploadToGyazo = async (path) => {
+  const data = await fetch(`https://upload.gyazo.com/api/upload`, {
+    method: "POST",
+    body: buildFormData(path),
+  });
+  return data.json();
+};
+
+const run = () => {
+  watcher.on("add", async (event, _) => {
+    const data = await uploadToGyazo(event);
+    if (data?.url) {
+      clipboard.writeSync(data.url);
+    }
+  });
+};
+
+run();
